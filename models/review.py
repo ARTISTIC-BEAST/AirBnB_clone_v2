24e1a9e64918b26c9f00c29186cc032619bb3b7a
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-08-22 13:34:32 -0700

    import errors fixed

--- models/review.py
+++ models/review.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from models import storage_type
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from uuid import uuid4
+from os import getenv
 
-class Review(BaseModel):
-    """ Review class to store review information """
-      __tablename__ = 'reviews'
-    if storage_type == 'db':
+type_storage = getenv('HBNB_TYPE_STORAGE')
+
+
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    if type_storage == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -16,7 +18,3 @@ class Review(BaseModel):
         place_id = ""
         user_id = ""
         text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializing the Review"""
-        super().__init__(*args, **kwargs)
