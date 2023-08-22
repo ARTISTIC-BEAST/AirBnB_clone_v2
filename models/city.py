e66695520ec70febb7189f2866472addd04ead17
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-08-22 13:29:27 -0700

    full storage engine implemented

--- models/city.py
+++ models/city.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models import storage_type
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 from sqlalchemy.orm import relationship
 
-class City(BaseModel):
+type_storage = getenv('HBNB_TYPE_STORAGE')
+
+
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-      __tablename__ = 'cities'
-    if storage_type == 'db':
+    __tablename__ = 'cities'
+    if type_storage == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
+        places = relationship('Place', backref='cities', cascade='all, delete')
     else:
         state_id = ""
         name = ""
-
-      def __init__(self, *args, **kwargs):
-        """initializing the city class """
-        super().__init__(*args, **kwargs)
