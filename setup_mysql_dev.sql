da5d53c8ed59be9967cee9b53fc96c956bb10a39
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-08-22 06:42:16 -0700

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,6 @@
--- script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+--creates a database a new user with specified permissions
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
